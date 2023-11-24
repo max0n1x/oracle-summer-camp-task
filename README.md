575f862d38a2fd4cece4584b9ac1dc6f80b366fc
Maksym Podhornyi <forjant1@gmail.com>
2023-11-24 17:06:24 +0100

    update main and added readme

---
+++ README.md
@@ -0,0 +1,4 @@
+# Task for Oracle NetSuite's Winter Camp 
+
+## Assigment: 
+Write a program in Java/JavaScript that  prints out the numbers 0 to 100 (without boundaries). If the number is even, print "Baz" instead of the number. If it's divisible by 5,  print "inga!". If both conditions are met, write both strings concatenated.
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // creation of strings to be printed
+        String even = "Baz";
+        String divsbyfive = "inga!";
+
+        // loop through 1 to 100
+        for (int i = 0; i <= 100; i++) {
+
+            // if number is divisible by 2 and 5 print concatenated string
+            if(((i % 2) == 0) && ((i % 5) == 0)){
+                System.out.println(even + divsbyfive);
+
+            // if number is divisible by 2 print even string
+            } else if((i % 2) == 0){
+                System.out.println(even);
+
+            // if number is divisible by 5 print divsbyfive string
+            } else if ((i % 5) == 0) {
+                System.out.println(divsbyfive);
+
+            // else print number
+            } else {
+                System.out.println(i);
+            }
+        }
+
     }
 }
